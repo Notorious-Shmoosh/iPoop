914ed19b15885dc5b6d0e5000632e0079d47101d
Yan Fan <yan.fan@ayasdi.com>
2016-02-18 07:54:21 -0800

    Deleted useless shit

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 import React, { AppRegistry, Component, StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 
